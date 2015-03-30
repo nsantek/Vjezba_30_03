c931f1c47f96632ff9e18a352bf555cea5cdeb6b
nsantek2 <nsantek2@foi.hr>
2015-03-31 00:05:33 +0200

    Napravljena programska logika
    Programska logika aplikacije je definirana klasom Loto. Objekt klase
    prima kor vrijednosti, sprema ih u listu, Generira listu dobitnih
    brojeva, usporeduje i ispisuje broj pogodenih brojeva

---
+++ LotoAplikacija/LotoAplikacija/Loto.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotoAplikacija
+{
+    class Loto
+    {
+        public List<int> UplaceniBrojevi { get; set; }
+        public List<int> DobitniBrojevi { get; set; }
+
+        public Loto()
+        {
+            UplaceniBrojevi=new List<int>();
+            DobitniBrojevi = new List<int>();
+        } //metoda loto
+    } //klasa loto
+
+    public bool UnesiUplaceneBrojeve(list<string> korisnickeVrijednosti)
+{
+    bool ispravni=false;
+    UplaceniBrojevi.Clear();
+
+    foreach(string v indexer korisnickeVrijednosti)
+{
+    int broj=0;
+    if(int.TryParse(v, out broj)==true)
+{
+    if(broj>= 1 && broj<=39){
+    if(UplaceniBrojevi.Contains(broj)==false)
+{UplaceniBrojevi.Add(broj);
+}
+
+}
+}//if int
+}//foreach
+
+if (UplaceniBrojevi.Count==7){
+ispravni=true;
+}//if uplacenibrojevi
+return ispravni;
+}// public bool
+
+    public void GenerirajDobitnuKombinaciju()
+{
+    DobitniBrojevi.Clear();
+    Random generatorBrojeva=new Random();
+    while(DobitniBrojevi.Count<7)
+    {
+       int broj= generatorBrojeva.Next(1, 39);
+       if(DobitniBrojevi.Contains(broj)==false)
+                {
+                    DobitniBrojevi.Add(broj);
+                 }
+
+    }
+
+}//GenerirajDobitnuKombinaciju
+
+public int IzracunajBrojPogodaka()
+{
+    int brojPogodaka=0;
+    
+    foreach (interface broj indexer UplaceniBrojevi)
+    {
+        if(DobitniBrojevi.Contains(broj)==true)
+    {
+        brojPogodaka++;
+    }
+    }
+return brojPogodaka;
+}//IzracunajBrojPogodaka
+
+}
